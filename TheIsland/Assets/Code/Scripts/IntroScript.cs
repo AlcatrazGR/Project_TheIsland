a1cf6c18365829a8d9d2d4222677737161c43d0d
AlcatrazGR <alcatrazgr0@gmail.com>
2015-01-17 11:37:56 +0200

    Initiallization of the project

---
+++ TheIsland/Assets/Code/Scripts/IntroScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroScript : MonoBehaviour {
+	public Texture2D beginSplash;
+
+	[HideInInspector]
+	public IntroScript gameDataRef;
+
+	// Use this for initialization
+	void Start () {
+		gameDataRef = GetComponent<IntroScript> ();
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.gameDataRef.beginSplash, ScaleMode.ScaleToFit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
