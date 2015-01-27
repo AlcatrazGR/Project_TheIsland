8b82f4647ba9a4d0b0c1e7dfaa77d5729a1dfae7
AlcatrazGR <alcatrazgr0@gmail.com>
2015-01-27 17:15:57 +0200

    Main Menu me duo epiloges kai allagh grammatoseiras

--- TheIsland/Assets/Code/Scripts/IntroScript.cs
+++ TheIsland/Assets/Code/Scripts/IntroScript.cs
@@ -2,19 +2,33 @@
 using System.Collections;
 
 public class IntroScript : MonoBehaviour {
-	public Texture2D beginSplash;
-
-	[HideInInspector]
-	public IntroScript gameDataRef;
+	public bool isQuit = false;
 
 	// Use this for initialization
 	void Start () {
-		//gameDataRef = GetComponent<IntroScript> ();
-		//GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.gameDataRef.beginSplash, ScaleMode.ScaleToFit);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseEnter(){
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseDown(){
+		if (isQuit) {
+			Debug.Log("On quit");
+			Application.Quit ();
+		} else {
+			// Application.LoadLevel(1); Make sure that level "1" is located in your build settings. You can also change the 1 with a name of your scene.
+		}
+
 	}
 }
