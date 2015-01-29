7e5b84e3c405c8b8269de5b62455a496b16a008a
AlcatrazGR <alcatrazgr0@gmail.com>
2015-01-29 10:03:12 +0200

    Prosthesi flash light kai katallhlo script

---
+++ TheIsland/Assets/Code/Scripts/FlashLightControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLightControl : MonoBehaviour {
+
+	public Light flashLightObj;
+
+	// Use this for initialization
+	void Start () {
+		flashLightObj.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if(flashLightObj.enabled == false){
+				flashLightObj.enabled = true;
+			}
+			else{
+				flashLightObj.enabled = false;
+			}
+
+		}
+	}
+}
